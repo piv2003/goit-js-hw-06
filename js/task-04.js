cfe6b7f4573213f285375fe97fa9c13962af6dc9
Ihor Poliakov <piv2003@gmail.com>
2022-12-09 20:51:57 +0200

    adding Task 4

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const value = document.querySelector('#value')
+
+const btnDecr = document.querySelector('button[data-action="decrement"]')
+const btnIncr =  document.querySelector('button[data-action="increment"]')
+
+let counterValue = Number(document.querySelector('#value').textContent)
+
+btnDecr.addEventListener('click', minus);
+btnIncr.addEventListener('click', plus);
+
+function minus(evt) {
+    counterValue -= 1      
+    value.innerHTML = counterValue
+}
+
+function plus(evt) {
+    counterValue += 1       
+    value.innerHTML = counterValue
+}
+
+//console.dir(counterValue)
