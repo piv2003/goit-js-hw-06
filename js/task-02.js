3468fcc48037f98137b2569f800ff5f97e44db4a
Ihor Poliakov <piv2003@gmail.com>
2022-12-09 18:00:59 +0200

    adding task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListElem = document.querySelector('#ingredients');
+
+const ingredientsItemArr = ingredients.map(ingredient => {
+  const ingredientsElem = document.createElement('li');
+  ingredientsElem.classList.add('item');
+  ingredientsElem.textContent = ingredient;
+  return ingredientsElem;
+})
+ingredientsListElem.append(...ingredientsItemArr);
+
+console.log(ingredientsListEl);
