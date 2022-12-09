cb563d4a90d351fff3e4025c86be4a8c1f072ec1
Ihor Poliakov <piv2003@gmail.com>
2022-12-10 00:03:16 +0200

    adding Task 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#validation-input')
+
+input.addEventListener('blur', valid)
+
+function valid(evt) { 
+    if (evt.currentTarget.value.length === Number(input.dataset.length)) {
+        input.classList.remove("invalid");
+        input.classList.add("valid");
+    }
+     else {
+        input.classList.remove("valid");   
+        input.classList.add("invalid");
+    } 
+}
