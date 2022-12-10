799a989e789bbd2820e9542af0570c5ae18e0ee1
Ihor Poliakov <piv2003@gmail.com>
2022-12-10 12:58:14 +0200

    adding Task - 10 (partially)

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputBoxes = document.querySelector('input');
+const divBoxes = document.querySelector('#boxes');
+const createBoxes = document.querySelector('[data-create]'); 
+const destroyBoxes = document.querySelector('[data-destroy]'); 
+
+createBoxes.addEventListener("click", createBoxes);
+destroyBoxes.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+
+};
+
+function destroyBoxes() {
+  
+};
+
+
