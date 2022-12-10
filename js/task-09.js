38e9f3b336ad46c8ba86fd6463381d47e72a890b
Ihor Poliakov <piv2003@gmail.com>
2022-12-10 12:56:29 +0200

    adding Task - 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyBackground = document.querySelector('body');
+const spanColorText = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", onchangeColor);
+
+function onchangeColor() {
+  bodyBackground.style.backgroundColor = getRandomHexColor();
+  spanColorText.textContent = getRandomHexColor();
+}
