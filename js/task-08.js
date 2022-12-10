696354998aa4e702a68af18361f8e693ed189729
Ihor Poliakov <piv2003@gmail.com>
2022-12-10 10:59:38 +0200

    adding Task - 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener('submit', validForm);
+
+function validForm(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password } } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert('Please fill in all fields');
+        return;
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
 
